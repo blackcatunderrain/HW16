9a8590a5143f8421a917af4c93ccadbc8d254639
blackcatunderrain <a27v@outlook.com>
2022-08-05 14:44:21 +0300

    step 5

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import json
 from config import app
 from db_init import init_db
-from service import get_all_users
+from models import User, Order, Offer
+from service import get_all, get_all_by_id
 
 
 @app.route('/users/', methods=['GET'])
 def get_user():
-    get_all_users()
     return app.response_class(
-        response=json.dumps(get_all_users(), indent=4),
+        response=json.dumps(get_all(User), indent=4),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/orders/', methods=['GET'])
+def get_orders():
+    return app.response_class(
+        response=json.dumps(get_all(Order), indent=4),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/offers/', methods=['GET'])
+def get_offers():
+    return app.response_class(
+        response=json.dumps(get_all(Offer), indent=4),
         status=200,
         mimetype='application/json'
     )
@@ -16,16 +34,29 @@ def get_user():
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
-    data = get_all_users()
-    for raw in data:
-        if raw.get('id') == user_id:
-            return app.response_class(
-                response=json.dumps(raw, indent=4),
-                status=200,
-                mimetype='application/json'
-            )
+    data = get_all_by_id(User, user_id)
     return app.response_class(
-        response=json.dumps({}, indent=4),
+        response=json.dumps(data, indent=4),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/orders/<int:order_id>', methods=['GET'])
+def get_orders_by_id(order_id):
+    data = get_all_by_id(Order, order_id)
+    return app.response_class(
+        response=json.dumps(data, indent=4),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/offers/<int:offer_id>', methods=['GET'])
+def get_offers_by_id(offer_id):
+    data = get_all_by_id(Offer, offer_id)
+    return app.response_class(
+        response=json.dumps(data, indent=4),
         status=200,
         mimetype='application/json'
     )
--- service.py
+++ service.py
@@ -1,8 +1,15 @@
 from models import *
 
 
-def get_all_users():
+def get_all(model) -> list[dict]:
     result = []
-    for raw in User.query.all():
+    for raw in db.session.query(model).all():
         result.append(raw.to_dict())
     return result
+
+
+def get_all_by_id(model, find_id):
+    try:
+        return db.session.query(model).get(find_id).to_dict()
+    except Exception:
+        return {}
