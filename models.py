b67f6cb223d2e8b238dd311109a98625a6db5574
blackcatunderrain <a27v@outlook.com>
2022-08-05 12:29:40 +0300

    step 2 complete

--- models.py
+++ models.py
@@ -1,1 +1,63 @@
-from flask_sqlalchemy import SQLAlchemy
+from config import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(255))
+    role = db.Column(db.String(255))
+    phone = db.Column(db.String(255))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age,
+            'email': self.email,
+            'role': self.role,
+            'phone': self.phone
+        }
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    start_date = db.Column(db.Integer)
+    end_date = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'address': self.address,
+            'price': self.price,
+            'customer_id': self.customer_id,
+            'executor_id': self.executor_id
+        }
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.String(255), db.ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'order_id': self.order_id,
+            'executor_id': self.executor_id
+        }
