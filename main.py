3c0944f07bbd243f20e64aadb092a490f75f7272
blackcatunderrain <a27v@outlook.com>
2022-08-08 12:49:00 +0300

    final

--- main.py
+++ main.py
@@ -1,65 +1,150 @@
 import json
+from flask import request
 from config import app
-from db_init import init_db
+from db_init import init_db, insert_data_user, insert_data_order, insert_data_offer
 from models import User, Order, Offer
-from service import get_all, get_all_by_id
+from service import get_all, get_all_by_id, update_all, delete_all
 
 
-@app.route('/users/', methods=['GET'])
+@app.route('/users/', methods=['GET', 'POST'])
 def get_user():
+    if request.method == 'GET':
+        return app.response_class(
+            response=json.dumps(get_all(User), indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'POST':
+        if isinstance(request.json, list):
+            insert_data_user(request.json)
+        elif isinstance(request.json, dict):
+            insert_data_user([request.json])
+        else:
+            print("Invalid request method")
     return app.response_class(
-        response=json.dumps(get_all(User), indent=4),
+        response=json.dumps(request.json, indent=4),
         status=200,
         mimetype='application/json'
     )
 
 
-@app.route('/orders/', methods=['GET'])
+@app.route('/orders/', methods=['GET', 'POST'])
 def get_orders():
+    if request.method == 'GET':
+        return app.response_class(
+            response=json.dumps(get_all(Order), indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'POST':
+        if isinstance(request.json, list):
+            insert_data_order(request.json)
+        elif isinstance(request.json, dict):
+            insert_data_order([request.json])
+        else:
+            print("Invalid request method")
     return app.response_class(
-        response=json.dumps(get_all(Order), indent=4),
+        response=json.dumps(request.json, indent=4),
         status=200,
         mimetype='application/json'
     )
 
 
-@app.route('/offers/', methods=['GET'])
+@app.route('/offers/', methods=['GET', 'POST'])
 def get_offers():
+    if request.method == 'GET':
+        return app.response_class(
+            response=json.dumps(get_all(Offer), indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'POST':
+        if isinstance(request.json, list):
+            insert_data_offer(request.json)
+        elif isinstance(request.json, dict):
+            insert_data_offer([request.json])
+        else:
+            print("Invalid request method")
     return app.response_class(
-        response=json.dumps(get_all(Offer), indent=4),
+        response=json.dumps(request.json, indent=4),
         status=200,
         mimetype='application/json'
     )
 
 
-@app.route('/users/<int:user_id>', methods=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_user_by_id(user_id):
-    data = get_all_by_id(User, user_id)
-    return app.response_class(
-        response=json.dumps(data, indent=4),
-        status=200,
-        mimetype='application/json'
-    )
+    if request.method == 'GET':
+        data = get_all_by_id(User, user_id)
+        return app.response_class(
+            response=json.dumps(data, indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'PUT':
+        update_all(User, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_all(User, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
 
 
-@app.route('/orders/<int:order_id>', methods=['GET'])
-def get_orders_by_id(order_id):
-    data = get_all_by_id(Order, order_id)
-    return app.response_class(
-        response=json.dumps(data, indent=4),
-        status=200,
-        mimetype='application/json'
-    )
+@app.route('/orders/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_orders_by_id(user_id):
+    if request.method == 'GET':
+        data = get_all_by_id(Order, user_id)
+        return app.response_class(
+            response=json.dumps(data, indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'PUT':
+        update_all(Order, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_all(Order, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
 
 
-@app.route('/offers/<int:offer_id>', methods=['GET'])
-def get_offers_by_id(offer_id):
-    data = get_all_by_id(Offer, offer_id)
-    return app.response_class(
-        response=json.dumps(data, indent=4),
-        status=200,
-        mimetype='application/json'
-    )
+@app.route('/offers/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_offers_by_id(user_id):
+    if request.method == 'GET':
+        data = get_all_by_id(Offer, user_id)
+        return app.response_class(
+            response=json.dumps(data, indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'PUT':
+        update_all(Offer, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_all(Offer, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(['OK'], indent=4),
+            status=200,
+            mimetype='application/json'
+        )
 
 
 if __name__ == '__main__':
--- service.py
+++ service.py
@@ -13,3 +13,19 @@ def get_all_by_id(model, find_id):
         return db.session.query(model).get(find_id).to_dict()
     except Exception:
         return {}
+
+
+def update_all(model, user_id, values):
+    try:
+        db.session.query(model).filter(model.id == user_id).update(values)
+        db.session.commit()
+    except Exception:
+        return {}
+
+
+def delete_all(model, user_id, values):
+    try:
+        db.session.query(model).filter(model.id == user_id).delete()
+        db.session.commit()
+    except Exception:
+        return {}
